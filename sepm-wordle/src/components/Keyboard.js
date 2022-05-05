4f9c58e7aaec80088f50fecbab631273ca79fdae
sagar-datta <s3605651@student.rmit.edu.au>
2022-05-05 13:41:01 +0800

    can use keyboard to enter word

--- sepm-wordle/src/components/Keyboard.js
+++ sepm-wordle/src/components/Keyboard.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useCallback, useEffect, useContext } from "react";
 import Key from "./Key";
+import { AppContext } from "../App";
 
 function Keyboard() {
+  const { onEnterClick, onDeleteClick, onPickLetter } = useContext(AppContext);
+
   const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
   const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
   const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
 
+  const handleKeyboard = useCallback((event) => {
+    if (event.key == "Enter") {
+      onEnterClick();
+    } else if (event.key == "Backspace") {
+      onDeleteClick();
+    } else {
+      keys1.forEach((key) => {
+        if (event.key.toUpperCase() === key.toUpperCase()) {
+          onPickLetter(key);
+        }
+      });
+      keys2.forEach((key) => {
+        if (event.key.toUpperCase() === key.toUpperCase()) {
+          onPickLetter(key);
+        }
+      });
+      keys3.forEach((key) => {
+        if (event.key.toUpperCase() === key.toUpperCase()) {
+          onPickLetter(key);
+        }
+      });
+    }
+  });
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyboard);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyboard);
+    };
+  }, [handleKeyboard]);
+
   return (
-    <div className="keyboard">
+    <div className="keyboard" onKeyDown={handleKeyboard}>
       <div className="firstLine">
         {keys1.map((key) => {
           return <Key keyval={key} />;
