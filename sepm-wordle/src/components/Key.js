17bcdda1862a1e56d3205c62b93d1b4aa0de41aa
sagar-datta <s3605651@student.rmit.edu.au>
2022-05-05 13:14:06 +0800

    can input letters and enter to new line

--- sepm-wordle/src/components/Key.js
+++ sepm-wordle/src/components/Key.js
@@ -2,12 +2,26 @@ import React, { useContext } from "react";
 import { AppContext } from "../App";
 
 function Key({ keyval, functionKey }) {
-  const { board, setBoard, currentAttempt, setCurrentAttempt } = useContext(AppContext);
+  const { board, setBoard, currentAttempt, setCurrentAttempt } =
+    useContext(AppContext);
+
   const pickLetter = () => {
-    const newBoard = [...board];
-    newBoard[currentAttempt.attempt][currentAttempt.letterPosition] = keyval;
-    setBoard(newBoard);
-    setCurrentAttempt({ ...currentAttempt, letterPosition: currentAttempt.letterPosition + 1 });
+    if (keyval === "ENTER") {
+      if (currentAttempt.letterPosition !== 5) return;
+      setCurrentAttempt({
+        attempt: currentAttempt.attempt + 1,
+        letterPosition: 0,
+      });
+    } else {
+      if (currentAttempt.letterPosition > 4) return;
+      const newBoard = [...board];
+      newBoard[currentAttempt.attempt][currentAttempt.letterPosition] = keyval;
+      setBoard(newBoard);
+      setCurrentAttempt({
+        ...currentAttempt,
+        letterPosition: currentAttempt.letterPosition + 1,
+      });
+    }
   };
   return (
     <div className="key" id={functionKey && "functionKey"} onClick={pickLetter}>
