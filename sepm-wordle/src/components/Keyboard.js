3c8a3a27638b6e2e0ea9f4c51d18e35f83199a6d
sagar-datta <s3605651@student.rmit.edu.au>
2022-05-05 12:40:29 +0800

    have letters of keyboard showing on site

--- sepm-wordle/src/components/Keyboard.js
+++ sepm-wordle/src/components/Keyboard.js
@@ -1,14 +1,29 @@
 import React from "react";
 
 function Keyboard() {
-    const keys1 = ["Q","W","E","R","T","Y","U","I","O","P"];
-    const keys3 = ["A","S","D","F","G","H","J","K","L"];
-    const keys4 = ["Z","X","C","V","B","N","M"];
+  const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+  const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+  const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
+
   return (
     <div className="keyboard">
-      <div className="firstLine"></div>
-      <div className="secondLine"></div>
-      <div className="thirdLine"></div>
+      <div className="firstLine">
+        {keys1.map((key) => {
+          return <div>{key}</div>;
+        })}
+      </div>
+      <div className="secondLine">
+        {" "}
+        {keys2.map((key) => {
+          return <div>{key}</div>;
+        })}
+      </div>
+      <div className="thirdLine">
+        {" "}
+        {keys3.map((key) => {
+          return <div>{key}</div>;
+        })}
+      </div>
     </div>
   );
 }
