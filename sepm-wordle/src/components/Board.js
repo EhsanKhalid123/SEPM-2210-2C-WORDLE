ccd782512483e26358d442f670bffe41185943e8
sagar-datta <s3605651@student.rmit.edu.au>
2022-05-05 11:13:34 +0800

    setup board component skeleton

--- sepm-wordle/src/components/Board.js
+++ sepm-wordle/src/components/Board.js
@@ -2,8 +2,17 @@ import React, { useState } from "react";
 import { boardClean } from "./Words";
 
 function Board() {
-    const [board, setBoard] = useState(boardClean)
-  return <div>Board</div>;
+  const [board, setBoard] = useState(boardClean);
+  return (
+    <div className="Board">
+      <div className="row"></div>
+      <div className="row"></div>
+      <div className="row"></div>
+      <div className="row"></div>
+      <div className="row"></div>
+      <div className="row"></div>
+    </div>
+  );
 }
 
 export default Board;
