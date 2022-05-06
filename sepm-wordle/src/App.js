2b568ea40481c3233b2b2ad3bef8c737e78ed138
Sagar Datta <sagar.datta@student.rmit.edu.au>
2022-05-06 14:35:44 +0800

    completed HIGH CONTRAST MODE FUNCTIONALITY

--- sepm-wordle/src/App.js
+++ sepm-wordle/src/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
 import "./App.css";
+import "./index.css";
 import Board from "./components/Board";
 import Keyboard from "./components/Keyboard";
 import { createContext, useState } from "react";
 import { boardClean, generateGuessSet, randomlySelectAnswer } from "./Tools";
 import EndScreen from "./components/EndScreen";
-import answerList from "./test-answer-list.txt";
 {
   /* Allows the functions here to be called in other components */
 }
@@ -139,11 +139,35 @@ function App() {
       setGameCompleted({ gameCompleted: true, guessedWord: false });
     }
   };
+
+  const r = document.querySelector(":root");
+
+  const setHighContrastDark = () => {
+    r.style.setProperty("--colour-correct", "#e5804a");
+    r.style.setProperty("--colour-partial", "#92bef4");
+    r.style.setProperty("--background", "#121213");
+    r.style.setProperty("--textColour", "white");
+  };
+
+  const setDarkMode = () => {
+    r.style.setProperty("--colour-correct", "#618b55");
+    r.style.setProperty("--colour-partial", "#b1a04c");
+    r.style.setProperty("--background", "#121213");
+    r.style.setProperty("--textColour", "white");
+  };
+
   return (
     <div className="App">
       <nav>
         <h1>wordo</h1>
       </nav>
+      <button id="button" onClick={setHighContrastDark}>
+        High Contrast Theme
+      </button>
+      <button id="button" onClick={setDarkMode}>
+        Normal Theme
+      </button>
+
       {/** The following values are being used by other components in the project */}
       <AppContext.Provider
         value={{
