18fa705004777f193d56ee88430b830c670be50e
Sagar Datta <sagar.datta@student.rmit.edu.au>
2022-05-11 17:54:54 +0800

    trying to get countdown till midnight to work

--- src/components/EndScreen.js
+++ src/components/EndScreen.js
@@ -4,6 +4,17 @@ import { AppContext } from "../App";
 function EndScreen() {
   const { gameCompleted, setGameCompleted, answer, currentAttempt } =
     useContext(AppContext);
+
+
+  
+
+
+  const [counter, setCounter] = React.useState(60);
+
+  React.useEffect(() => {
+    counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+  }, [counter]);
+
   {
     /** Set gameover screen, can be either win or lose. Very basic, but does the trick
         and replaces the keyboard */
@@ -17,7 +28,13 @@ function EndScreen() {
       </h1>
       <h3>The correct word is: "{answer}"</h3>
       {gameCompleted.guessedWord && (
-        <h4>You guessed in {currentAttempt.attempt} attempt(s)</h4>
+        <h4>
+          You guessed in {currentAttempt.attempt} attempt(s)
+          <h4>
+            {" "}
+            <div>Next word of the day available in: {counter}</div>
+          </h4>
+        </h4>
       )}
     </div>
   );
