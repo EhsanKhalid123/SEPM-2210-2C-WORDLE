4e4e292c900e82d869eb2c6bc43aeb4d1273ce64
Sagar Datta <sagar.datta@student.rmit.edu.au>
2022-05-16 18:18:00 +0800

    cleaned up Letter.js

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -15,148 +15,6 @@ function Letter({ letterPosition, attemptValue }) {
     /** Initialise correct variable if the answer is in the position */
   }
 
-  {
-    /** Initialise partial variable if the answer is not in the position,
-     * but is includes in the word */
-  }
-  // const [repeat, setRepeat] = useState(false);
-  // const [repeatChar, setRepeatChar] = useState([]);
-  // const [repeatCount, setRepeatCount] = useState(0);
-  // const [answerRepeat, setAnswerRepeat] = useState(0);
-
-  // const [repeatCorrect, setRepeatCorrect] = useState(false);
-  // const [repeatPartial, setRepeatPartial] = useState(false);
-
-  // console.log(guess);
-  // console.log(answer);
-
-  // useEffect(() => {
-  //   var guessTest = guess.split("");
-  //   setRepeat(
-  //     guessTest.some(function (v, i, a) {
-  //       return a.lastIndexOf(v) != i;
-  //     })
-  //   );
-
-  //   const getRepeatesChars = (str) => {
-  //     const chars = {};
-  //     for (const char of str) {
-  //       chars[char] = (chars[char] || 0) + 1;
-  //     }
-  //     return Object.entries(chars)
-  //       .filter((char) => char[1] > 1)
-  //       .map((char) => char[0]);
-  //   };
-  //   setRepeatChar(getRepeatesChars(guess));
-  // }, [guess]);
-
-  // useEffect(() => {
-  //   function countString(str, letterCheck) {
-  //     let count = 0;
-  //     for (let i = 0; i < str.length; i++) {
-  //       if (str.charAt(i) == letterCheck) {
-  //         count += 1;
-  //       }
-  //     }
-  //     return count;
-  //   }
-
-  //   setRepeatCount(countString(guess, repeatChar.toString().charAt(0)));
-  // });
-  // useEffect(() => {
-  //   function countString(str, letterCheck) {
-  //     let count = 0;
-  //     for (let i = 0; i < str.length; i++) {
-  //       if (str.charAt(i) == letterCheck) {
-  //         count += 1;
-  //       }
-  //     }
-  //     return count;
-  //   }
-
-  //   setAnswerRepeat(countString(answer, repeatChar.toString().charAt(0)));
-  // });
-  // useEffect(() => {
-  //   for (let i = 0; i < 5; i++) {
-  //     // console.log(answer.toUpperCase()[i]);
-  //     // console.log(guess.toUpperCase()[i]);
-  //     // console.log(repeatChar.toString().charAt(0));
-  //     if (answer.toUpperCase()[i] === guess.toUpperCase()[i]) {
-  //       setRepeatCorrect(true);
-  //     } else {
-  //       setRepeatCorrect(false);
-  //     }
-  //   }
-  // });
-
-  // useEffect(() => {
-  //   let count = 0;
-  //   for (let i = 0; i < 5; i++) {
-  //     // console.log(answer.toUpperCase()[i]);
-  //     // console.log(guess.toUpperCase()[i]);
-  //     // console.log(repeatChar.toString().charAt(0));
-  //     if (
-  //       (answer.toUpperCase()[i] === guess.toUpperCase()[i]) ===
-  //       repeatChar.toString().charAt(0)
-  //     ) {
-  //       count++;
-  //     }
-  //   }
-  //   if (count > 0) {
-  //     setRepeatCorrect(true);
-  //   } else {
-  //     setRepeatCorrect(false);
-  //   }
-  //   console.log(count);
-  // });
-
-  // useEffect(() => {
-  //   if (repeatCount > 0) {
-  //     setRepeatPartial(true);
-  //   } else {
-  //     setRepeatPartial(false);
-  //   }
-  // });
-
-  // console.log(repeatChar.toString().charAt(0));
-  // console.log(repeatCount);
-  // console.log(repeatCorrect);
-  // console.log(repeatPartial);
-  // console.log(answerRepeat);
-  // console.log(currentAttempt.attempt);
-  // console.log(board[currentAttempt.attempt][letterPosition]);
-  // console.log(guess.indexOf(repeatChar.toString().charAt(0)));
-  // console.log(letter);
-
-  //
-  // var partial = "";
-  // if (2 === 2) {
-
-  // partial = !correct;
-  // partial = letter !== "";
-  // function renderReplace(component, container) {
-  //   var temp = document.createElement("div");
-  //   var React = require("react-dom");
-  //   React.render(component, temp);
-  //   container.parentNode.replaceChild(temp.querySelector("#div"), container);
-  // }
-
-  // for (let i = 0; i < 6; i++) {
-  //   for (let j = 0; j < 5; j++) {
-  //     console.log(board[i][j]);
-  //     if (answer.toUpperCase().includes(board[i][j])) {
-  //       console.log("yello");
-  //     }
-  //   }
-  // }
-
-  {
-    /** Sets the letterState one of three ways */
-  }
-  // useEffect(() => {
-  //   let mmmm = currentAttempt.attempt - 1;
-  //   let ssss = repeatArr[0];
-  // });
   var correct = false;
   if (answer.toUpperCase()[letterPosition] === letter) {
     if (currentAttempt.attempt > attemptValue) {
@@ -170,80 +28,20 @@ function Letter({ letterPosition, attemptValue }) {
     }
   }
 
-  // there are no repeated letter in answer, but the guess has multiple repeats
-  // there are no repeated letter in answer, but the guess has multiple repeats
-  // there are no repeated letter in answer, but the guess has multiple repeats
-  // var repeatLetter = "";
-  // if (answerRepeat === 0 && repeatCount > 0) {
-  //   // for (var i = 0; i < board.length; i++) {
-  //   //   var boardy = board[i];
-  //   //   for (var j = 0; j < boardy.length; j++) {
-  //   // }
-
-  //   // MAKES ARRAY OF INDEXES THAT HAVE THE REPEATED LETTER IN THEM
-  //   var repeatArr = [];
-  //   for (var j = 0; j < 6; j++) {
-  //     for (var i = 0; i < 5; i++) {
-  //       if (board[j][i] === repeatChar.toString().charAt(0)) {
-  //         repeatArr.push(i);
-  //       }
-  //     }
-  //   }
-
-  //   // CHECKS IF IT IS THE SAME ADD NEXT IF IT INCLUDES THE LETTER
-  //   var noRepeatArr = [];
-  //   for (var j = 0; j < 6; j++) {
-  //     for (var i = 0; i < 5; i++) {
-  //       if (
-  //         board[j][i] !== repeatChar.toString().charAt(0) &&
-  //         board[j][i] !== "" &&
-  //         answer.toUpperCase().includes(board[j][i])
-  //       ) {
-  //         noRepeatArr.push(i);
-  //       }
-  //     }
-  //   }
-  //   // console.log(noRepeatArr);
-
-  //   // DELETES A LETTER FROM THE ARRAY THAT IS ACTUALLY CORRECT AND NOT PARTIAL
-  //   if (repeatCorrect === true) {
-  //     for (var k = 0; k < answer.length; k++) {
-  //       if (repeatChar.toString().charAt(0) === answer.toUpperCase()[k]) {
-  //         // console.log(k);
-  //         for (var m = 0; m < repeatArr.length; m++) {
-  //           if (k === repeatArr[m]) {
-  //             repeatArr = repeatArr.filter((element) => element !== k);
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  //   console.log(repeatArr);
-
-
-  //   // console.log(board[currentAttempt.attempt - 1][repeatArr[0]]);
-  // }
-
   let partial = false;
-  if (
-    answer.toUpperCase().includes(letter) &&
-    !correct &&
-    letter !== ""
-  ) {
+  if (answer.toUpperCase().includes(letter) && !correct && letter !== "") {
     if (currentAttempt.attempt > attemptValue) {
-
-        letterState = "partial";
-        partial = true;
-        return (
-          <React.Fragment>
-            <div className="letter" id={letterState}>
-              {letter}
-            </div>
-          </React.Fragment>
-        );
-      
+      letterState = "partial";
+      partial = true;
+      return (
+        <React.Fragment>
+          <div className="letter" id={letterState}>
+            {letter}
+          </div>
+        </React.Fragment>
+      );
     }
-  } 
+  }
 
   if (
     currentAttempt.attempt > attemptValue &&
