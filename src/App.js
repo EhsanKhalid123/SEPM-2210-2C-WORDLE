2d352339ccd27cd874614f4eb5bbf804baf5bc37
Sagar Datta <sagar.datta@student.rmit.edu.au>
2022-05-21 12:56:44 +0800

    FINAL

--- src/App.js
+++ src/App.js
@@ -54,8 +54,8 @@ function App() {
       setGuessSet(words.guessSet);
     });
     getWordOfTheDay().then((words) => {
-      // setAnswer(words.wordOfTheDay);
-      setAnswer("DECOY")
+      setAnswer(words.wordOfTheDay);
+      // setAnswer("DECOY")
       setDiffInDays(words.diffInDays);
     });
   }, []);
